a9c8fbcc5388a5554e8d977b388d5419801f1f25
Nico Swiatecki <nico@devninjas.io>
2024-06-20 16:31:53 +0200

    ✨🔧 Update app version to 1.1.0 and imagePullPolicy to Always
    - Updated the app version in `.k8s/app.yaml` and `.k8s/ingress.yaml` from "1.0.0" to "1.1.0".
    - Added `imagePullPolicy: Always` for the container in `.k8s/app.yaml`.
    - Added environment variables (`POD_NAME`, `POD_NAMESPACE`, `POD_IP`, `NODE_NAME`) with their respective values in `GreetingController.java`.
    - Modified the response message in the `/` endpoint of `GreetingController.java` to include pod information.

--- src/main/java/com/devninjas/helloworld/GreetingController.java
+++ src/main/java/com/devninjas/helloworld/GreetingController.java
@@ -1,14 +1,26 @@
 package  com.devninjas.helloworld;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+    @Value("${POD_NAME:UNKNOWN_POD_NAME}")
+    private String podName;
+
+    @Value("${POD_NAMESPACE:UNKNOWN_POD_NAMESPACE}")
+    private String podNamespace;
+
+    @Value("${POD_IP:UNKNOWN_POD_IP}")
+    private String podIp;
+
+    @Value("${NODE_NAME:UNKNOWN_NODE}")
+    private String nodeName;
 
     @GetMapping("/")
     public String root() {
-        return "";
+        return String.format("Hello from %s/%s (%s), my home is %s", podName, podNamespace, podIp, nodeName);
     }
 
     @GetMapping("/hello")
